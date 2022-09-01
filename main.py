36bf89e750fc86add8aff9b51248ba7e0e25f1eb
joaofmds <ms.joao.felipe@gmail.com>
2022-09-01 15:47:23 -0300

    Passo 1: Desenvolvimento da função get_username para realizar a requisição da API do Github

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import unittest
+import requests
 
 def get_username(username: str) -> dict:
-    ...
+    url = f'https://api.github.com/users/{username}'
+    r = requests.get(url)
+    if r.status_code == 200:
+        return print(r.json())
+    else:
+        return print(r.status_code)
 
 class User: ...
 
